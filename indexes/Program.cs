81f135b72b41743b480749964033af1ce7e12683
Irina <cg.irenecooper@gmail.com>
2019-01-13 20:40:03 +0200

    first commit

---
+++ indexes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace indexes
+{
+    /**
+        Домашнее задание:
+        Создать таблицу содержащую как минимум одно текстовым полем.
+        Заполнить таблицу не менее 10000 случайными, но повторяющимися записями (можно воспользоваться, например, сервисом https://www.mockaroo.com)
+        Сделать выборку для поиска количества повторений одной из записи - замерять общее время выполнения операции.
+        Добавить индекс к текстовому полю, выполнить повторую выборку и сравнить время запроса с ранее полученным.
+        Сделать вывод об эффективности или не эффективности использования индекстов
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You shold just start this programm
+
+            TesterDB tester = new TesterDB("TSQL_HW_3_Kulikova", "Users", @".\Users.sql");
+            TesterDB testerIndexFirstName = new TesterDB("TSQL_HW_3_Kulikova_indexing_first_name", "Users", @".\Users_indexing_first_name.sql");
+
+            tester.TestMethod("Lurette");
+            testerIndexFirstName.TestMethod("Lurette");
+
+            Console.Read();
+        }
+    }
+}
---
+++ indexes/TesterDB.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace indexes
+{
+    /// <summary>
+    /// class for testing indexes varchar feild in ms sql databases
+    /// </summary>
+    public class TesterDB
+    {
+        public bool HasDataBase { get; private set; }
+        public bool HasTable { get; private set; }
+        public string ConnectionString { get; private set; }
+        public string SchemaName { get; private set; }
+        public string SQLFilePath { get; private set; }
+
+        public TesterDB(string schema, string table, string sql)
+        {
+            HasTable = false;
+            HasDataBase = false;
+            SchemaName = schema;
+            SQLFilePath = sql;
+            ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;";
+            CreateDB();
+            InitializationDB();
+        }
+
+        /// <summary>
+        /// create data base if not exists
+        /// </summary>
+        private void CreateDB()
+        {
+            if (!HasDataBase)
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string createDB = "IF NOT EXISTS ( SELECT * FROM master.dbo.sysdatabases WHERE name = '" + SchemaName + "') BEGIN CREATE DATABASE " + SchemaName +" END";
+                    using (var createDBCommand = new SqlCommand(createDB, connection))
+                    {
+                        var exists = createDBCommand.ExecuteNonQuery();
+                        HasDataBase = true;
+                        ConnectionString += "Initial Catalog = " + SchemaName + ";";
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// method form inizatilation table Users by data from sql file
+        /// </summary>
+        private void InitializationDB()
+        {
+            CheckTableExists();
+            if (!HasTable)
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    if (!HasTable)
+                    {
+                        var script = File.ReadAllText(SQLFilePath);
+                        var commandStrings = Regex.Split(script, @";", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+                        connection.Open();
+
+                        foreach (var commandString in commandStrings)
+                        {
+                            if (commandString.Trim() != "")
+                            {
+                                using (var command = new SqlCommand(commandString, connection))
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// check exists database
+        /// </summary>
+        private void CheckTableExists()
+        {
+            if (!HasTable)
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string DBandTableExists = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = '" + SchemaName + "'";
+                    using (var existsCommand = new SqlCommand(DBandTableExists, connection))
+                    {
+                        var exists = existsCommand.ExecuteReader();
+                        HasTable = exists.HasRows;
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        
+        /// <summary>
+        /// test performance, method selects from Users by name
+        /// </summary>
+        /// <param name="name"></param>
+        public void TestMethod(string name)
+        {
+            Stopwatch watcher = new Stopwatch();
+            watcher.Start();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var query = "SELECT * FROM Users WHERE first_name = '" + name + "'";
+                connection.Open();
+                using (var existsCommand = new SqlCommand(query, connection))
+                {
+                    var exists = existsCommand.ExecuteReader();
+                }
+                connection.Close();
+            }
+
+            watcher.Stop();
+            Console.WriteLine("Time elapsed: {0}", watcher.Elapsed);
+            Console.WriteLine(new string('-', 50));
+
+            Drop();
+        }
+
+        /// <summary>
+        /// droping database after testing
+        /// </summary>
+        private void Drop()
+        {
+            if (HasDataBase)
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string dropDB = "USE master IF EXISTS ( SELECT * FROM master.dbo.sysdatabases WHERE name = '" + SchemaName + "') BEGIN DROP DATABASE " + SchemaName + " END";
+                    using (var dropDBCommand = new SqlCommand(dropDB, connection))
+                    {
+                        var exists = dropDBCommand.ExecuteNonQuery();
+                        HasDataBase = false;
+                    }
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
